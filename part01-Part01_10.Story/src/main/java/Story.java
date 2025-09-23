61bbf0d7d9d2e666156850f85db9eb37970ef61c
matheus <suporte8@arcoinformatica.com>
2025-09-23 15:54:07 -0300

    Part 1 number 3 made

--- part01-Part01_10.Story/src/main/java/Story.java
+++ part01-Part01_10.Story/src/main/java/Story.java
@@ -7,6 +7,15 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        String name = scanner.nextLine();
+        System.out.println("What is their job?");
+        String jobName = scanner.nextLine();
+        System.out.println("Here is the story:  ");
+        System.out.println("Once upon a time there was "+ name +", who was "+ jobName +".");
+        System.out.println("On the way to work, "+ name + " reflected on life.");
+        System.out.println("Perhaps "+ name + " will not be "+ jobName +" forever." );
 
     }
 }
