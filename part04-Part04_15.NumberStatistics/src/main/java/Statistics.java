d08ad21454adb2bd353b89ac06c57bbd94bd3db4
matheus <matheusdeoliveiramota2020@gmail.com>
2025-12-09 07:44:34 -0300

    Update

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,24 @@
 
 public class Statistics {
+    private int count;
+    private int sum;
+
+    public Statistics(){
+         this.count = 0;
+         this.sum = 0;
+    }
+
+    public void addNumber(int number){
+        this.count++;
+        this.sum += number;
+    }
+    public int getCount(){
+        return count;
+    }
+    public int sum(){
+        return this.sum;
+    }
+    public double average(){
+        return Double.valueOf(this.sum / this.count);
+    }
 }
