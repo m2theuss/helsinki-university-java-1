121f809f025783666bf01bc13b2fae26c36ccdf5
matheus <matheusdeoliveiramota2020@gmail.com>
2025-12-15 20:20:23 -0300

    Update

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,22 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            System.out.print("First name: ");
+            String name = scanner.nextLine();
+            if(name.isEmpty()){
+                break;
+            }
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+
+            System.out.print("Identification number: ");
+            String indentNumber = scanner.nextLine();
+
+            infoCollection.add(new PersonalInformation(name,lastName,indentNumber));
+        }
+        for(PersonalInformation person : infoCollection){
+            System.out.println(person.getFirstName() + " " + person.getLastName());
+        }
     }
 }
