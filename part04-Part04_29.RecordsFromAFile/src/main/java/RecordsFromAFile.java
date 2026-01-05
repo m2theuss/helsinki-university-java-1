2e24c4b1fe0140cf883fba2e2bd98a64bcda3cc8
matheus <matheusdeoliveiramota2020@gmail.com>
2026-01-05 20:15:22 -0300

    Happy 2026

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,24 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Name of the file:");
+        String fileName = scanner.nextLine();
+        try(Scanner file = new Scanner(Paths.get(fileName))){
+            while(file.hasNextLine()){
+                String line = file.nextLine();
 
+                String[] values = line.split(",");
+                int number = Integer.valueOf(values[values.length - 1]);
+
+                System.out.print(values[0] + ", ");
+                if(number > 1){
+                    System.out.println("age: " + values[values.length - 1] + " years");
+                }else{
+                    System.out.println("age: " + values[values.length - 1] + " year");
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
