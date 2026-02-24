83debd3f44b90ef7aed979c0096f3bdf991604fd
matheus <matheusdeoliveiramota2020@gmail.com>
2026-02-23 21:01:22 -0300

    Update

---
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -0,0 +1,63 @@
+import java.lang.Math;
+public class Money {
+
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+
+        if (cents > 99) {
+            euros = euros + cents / 100;
+            cents = cents % 100;
+        }
+
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return this.euros;
+    }
+
+    public int cents() {
+        return this.cents;
+    }
+
+    @Override
+    public String toString() {
+        String zero = "";
+        if (this.cents < 10) {
+            zero = "0";
+        }
+
+        return this.euros + "." + zero + this.cents + "e";
+    }
+
+    public Money plus(Money addition){
+        return new Money(euros + addition.euros, cents + addition.cents);
+    }
+    public boolean lessThan(Money compared){
+
+        double totalCompared = (double)compared.euros + ((double)compared.cents / 10.0);
+        double totalThis = (double)euros + ((double)cents / 100.0);
+
+        if(totalThis < totalCompared){
+            return true;
+        }
+        return false;
+    }
+    public Money minus(Money decrease){
+        double tmpDecrease = (double)decrease.euros + ((double)decrease.cents / 100.0);
+        double totalThis = (double)euros + ((double)cents / 100.0);
+
+        double totalDecrease = totalThis - tmpDecrease;
+        double newCents = totalDecrease % 1;
+        double newEuros = totalDecrease - newCents;
+        if(totalDecrease < 0){
+            return new Money(0,0);
+        }
+        return new Money((int)newEuros, (int)Math.round(newCents));
+
+    }
+    
+}
