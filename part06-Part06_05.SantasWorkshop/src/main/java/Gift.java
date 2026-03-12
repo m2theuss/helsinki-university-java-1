2ebba532de833357d8525fd0e67707ac684dfa19
matheus <matheusdeoliveiramota2020@gmail.com>
2026-03-12 06:34:48 -0300

    Update

---
+++ part06-Part06_05.SantasWorkshop/src/main/java/Gift.java
@@ -0,0 +1,19 @@
+public class Gift{
+    private String name;
+    private int weight;
+
+    public Gift(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public String getName(){
+        return name;
+    }
+    @Override
+    public String toString(){
+        return name + " (" + weight + " kg) ";
+    }
+}
