3d81cc88f3647c01ad0a2846760da366a38fd56f
matheus <matheusdeoliveiramota2020@gmail.com>
2026-03-30 20:29:56 -0300

    On part 06 is missing just the take() method

---
+++ part06-Part06_07.HeightOrder/src/main/java/Room.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+public class Room{
+    private ArrayList<Person> personList;
+
+    public Room(){
+
+    }
+    public void add(Person person){
+        personList.add(person);
+    }
+    public boolean isEmpty(){
+        if(personList.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+    public ArrayList<Person> getPersons(){
+        return personList;
+    } 
+    public Person shortest(){
+        if(personList.isEmpty()){
+            return null;
+        }
+        Person shortestPerson = personList.get(0);
+        for(Person item : personList){
+            if(item.getHeight() < shortestPerson.getHeight()){
+                shortestPerson = item;
+            }
+        }
+        return shortestPerson;
+    }
+    public Person take(){
+        shortest();
+    }
+}
