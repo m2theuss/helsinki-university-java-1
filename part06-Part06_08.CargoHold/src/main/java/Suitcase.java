87ba8266ab451b02ed14b0ccdc1884a8f628c767
matheus <matheusdeoliveiramota2020@gmail.com>
2026-04-07 20:15:17 -0300

    The two methods from 'Cargo Hold' of 4 part has been implemented

---
+++ part06-Part06_08.CargoHold/src/main/java/Suitcase.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+public class Suitcase{
+    private ArrayList<Item> items;
+    private int maxWeight;
+
+    public Suitcase(int maxWeight){
+        this.items = new ArrayList<>();
+        this.maxWeight = maxWeight;
+    }
+    public void addItem(Item newItem){
+        int total = 0;
+        for(Item item : items){
+            total += item.getWeight();
+        }
+        if((total + newItem.getWeight()) > maxWeight){
+            return;
+        }
+        items.add(newItem);
+    }
+
+    @Override
+    public String toString(){
+        int totalWeight = 0;
+        for(Item item : items){
+            totalWeight += item.getWeight();
+        }
+        if(items.size() == 0){
+            return "no items (0 kg)";
+        }
+        if(items.size() == 1){
+            return "1 item " + "(" + totalWeight + "kg)";
+        }
+        return items.size() + " items " + "(" + totalWeight + "kg)";
+    }
+    public void printItems(){
+        for(Item item : items){
+            System.out.println(item.getName() + "(" + item.getWeight() + "kg)");
+        }
+    }
+    public int totalWeight(){
+        int total = 0;
+        for(Item item : items){
+            total += item.getWeight();
+        }
+        return total;
+    }
+}
