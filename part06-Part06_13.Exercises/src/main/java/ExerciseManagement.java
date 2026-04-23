e0f0990f162146a85b2c76c2c30a6069f96c04e6
matheus <matheusdeoliveiramota2020@gmail.com>
2026-04-22 21:19:21 -0300

    wip: implementing exercise management logic and tests with junit

--- part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,21 @@
-
+import java.util.ArrayList;
 public class ExerciseManagement {
+    private ArrayList<String> list;
 
+    public ExerciseManagement(){
+        this.list = new ArrayList<>();
+    }
+
+    public ArrayList<String> exerciseList(){
+        return this.list;
+    }
+    public void add(String value){
+        this.list.add(value);
+    }
+    public void markAsCompleted(String exercice){
+
+    }
+    public boolean isCompleted(String exercice){
+        return true;
+    }
 }
--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,33 @@
-
-
+import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class ExerciseManagementTest {
+    private ExerciseManagement management;
 
+    @Before
+    public void initialize(){
+        management = new ExerciseManagement();
+    }
+
+    @Test
+    public void exerciseListEmptyAtBeginning(){
+        assertEquals(0, management.exerciseList().size());
+    }
+    @Test
+    public void addingExerciseGrowsListByOne(){
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+    @Test
+    public void addedExercisesIsInList(){
+        management.add("My value");
+        assertTrue(management.exerciseList().contains("My value"));
+    }
+    @Test
+    public void exerciseCanBeMarkedAsCompleted(){
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertTrue(management.isCompleted("New exercise"));
+    }
 }
